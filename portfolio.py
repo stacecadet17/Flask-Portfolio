908308cff18a77c52c6999388b6cdff50037a75c
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-09-07 15:46:30 -0700

    first commit for portfolio flask assignment

---
+++ portfolio.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/aboutme')
+def about_me():
+    return render_template('aboutme.html')
+
+
+app.run(debug=True)
